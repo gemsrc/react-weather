02d4eaf5b65ebaed4596e763934af1e2b15ce3b1
gemsrc <70901687+gemsrc@users.noreply.github.com>
2020-12-06 20:32:45 +0100

    created css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Weather from "./Weather";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
--- src/Weather.js
+++ src/Weather.js
@@ -1,59 +1,58 @@
 import React from "react";
-export default function Weather(){
-    return (
-      <div className="Weather">
-        <form>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city..."
-                className="form-control"
-                autoFocus="on"
-              />
-            </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-outline-dark"
-              />
-            </div>
-          </div>
-        </form>
+import "./Weather.css";
 
-        <div className="City">
-          <h1>Pontevedra</h1>
-        </div>
-        <div>
-          <ul>
-            <li>Sunday 6th December </li>
-            <li> 23:30</li>
-          </ul>
-        </div>
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
         <div className="row">
-          <div className="col-6">
-            <h2>
-              6<sup>°</sup>
-              <small>C|F</small>
-            </h2>
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-outline-dark"
+            />
+          </div>
+        </div>
+      </form>
 
+      <div className="City">
+        <h1>Pontevedra</h1>
+      </div>
+      <div>
+        <p>Sunday 6th December || 22:30</p>
+    </div>
+      <div className="row">
+        <div className="col-6">
+          <div>
+            <span id="degree">
+              6 <sup>°</sup>
+              <small>C|F</small>
+            </span>
             <img
               src="http://openweathermap.org/img/wn/10d@2x.png"
               alt="Light Rain"
             ></img>
-          </div>
-
-          <div className="col-6">
-            <ul>
-              <li>Light Rain</li>
-              <li>feels like 5°C</li>
-              <li>Precipitation: 70%</li>
-              <li>Humidity: 25%</li>
-              <li>Wind: 15 km/h</li>
-            </ul>
+            
           </div>
         </div>
+
+        <div className="col-6">
+          <ul>
+            <li>Light Rain</li>
+            <li>Precipitation: 70%</li>
+            <li>Humidity: 25%</li>
+            <li>Wind: 15 km/h</li>
+          </ul>
+        </div>
       </div>
-    );
+    </div>
+  );
 }
