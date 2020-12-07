6bb82a3977bce9b1fbef6a7ed686f674827c75ad
gemsrc <70901687+gemsrc@users.noreply.github.com>
2020-12-07 13:12:33 +0100

    styled css and fixed positioning of some elements

--- src/Weather.js
+++ src/Weather.js
@@ -27,23 +27,9 @@ export default function Weather() {
         <h1>Pontevedra</h1>
       </div>
       <div>
-        <p>Sunday 6th December || 22:30</p>
-    </div>
+        <p id="dateTime">Sunday 6th December || 22:30</p>
+      </div>
       <div className="row">
-        <div className="col-6">
-          <div>
-            <span id="degree">
-              6 <sup>°</sup>
-              <small>C|F</small>
-            </span>
-            <img
-              src="http://openweathermap.org/img/wn/10d@2x.png"
-              alt="Light Rain"
-            ></img>
-            
-          </div>
-        </div>
-
         <div className="col-6">
           <ul>
             <li>Light Rain</li>
@@ -51,6 +37,19 @@ export default function Weather() {
             <li>Humidity: 25%</li>
             <li>Wind: 15 km/h</li>
           </ul>
+        </div>
+        <div className="col-6">
+          <div>
+            <span className="degree">6° </span>
+            <span className="unit">
+              C|F
+            </span>
+            <img
+              src="http://openweathermap.org/img/wn/10d@2x.png"
+              alt="Light Rain"
+              id="weatherImage"
+            ></img>
+          </div>
         </div>
       </div>
     </div>
